f80e24f010e4b443c1ba1ca2dc331d4ea4ffc17f
prathikpn14 <prathikpn14@gmail.com>
2026-09-22 15:49:02 +0530

    all changes

---
+++ Vtiger_TP (Copy)/src/test/java/demopackage1/DemoContact.java
@@ -0,0 +1,20 @@
+package demopackage1;
+
+import org.testng.annotations.Test;
+
+public class DemoContact {
+
+	@Test
+	public void m1()
+	{
+		String URL = System.getProperty("url" , "www.amazon.com");
+		String BROWSER = System.getProperty("browser" , "firefox");
+		String USERNAME = System.getProperty("username" , "");
+		String PASSWORD = System.getProperty("password" , "paasword");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+	}
+}
---
+++ Vtiger_TP (Copy)/src/test/java/demopackage2/DemoOrg.java
@@ -0,0 +1,17 @@
+package demopackage2;
+
+import org.testng.annotations.Test;
+
+public class DemoOrg {
+
+	@Test
+	public void m2()
+	{
+		System.out.println("ORG created");
+	}
+	@Test
+	public void m3()
+	{
+		System.out.println("ORG created 2");
+	}
+}
