aed5bfa33315eae1b0edeadcc5070b616653dd2e
prathikpn14 <prathikpn14@gmail.com>
2026-09-23 12:12:26 +0530

    modified demoOrg class

--- Vtiger_TP (Copy)/src/test/java/demopackage2/DemoOrg.java
+++ Vtiger_TP (Copy)/src/test/java/demopackage2/DemoOrg.java
@@ -8,6 +8,15 @@ public class DemoOrg {
 	public void m2()
 	{
 		System.out.println("ORG created");
+		String URL = System.getProperty("url" , "www.amazon.com");
+		String BROWSER = System.getProperty("browser" , "firefox");
+		String USERNAME = System.getProperty("username" , "yoyo");
+		String PASSWORD = System.getProperty("password" , "paasword");
+		
+		System.out.println("org url" + URL);
+		System.out.println("org browser" + BROWSER);
+		System.out.println("org username" + USERNAME);
+		System.out.println("org password" + PASSWORD);
 	}
 	@Test
 	public void m3()
